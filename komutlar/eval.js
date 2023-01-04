83775863c308f9ce22e60be3320d8b6075327d4b
toprak65 <80392076+toprak65@users.noreply.github.com>
2023-01-04 19:43:34 +0300

    Delete eval.js

--- komutlar/eval.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name:"eval",
-  aliases:"test",
-  description:"Kod denemenizi sağlar",
-  code: `$eval[$message]
-$onlyForIDs[$botOwnerID;Sahibim Değilsin?!]
-`
-} //komutlar dosyası komut taslağı
